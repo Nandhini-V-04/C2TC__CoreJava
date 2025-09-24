020c7520ab3709839ff4b831cec68f15d5c53e35
nandhini <nandhini@DESKTOP-UCNVJSL>
2025-09-25 00:19:38 +0530

    updated files

--- CoreJavaProject/src/Day1/SampleDemo.java
+++ CoreJavaProject/src/Day1/SampleDemo.java
@@ -5,7 +5,7 @@ public class SampleDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("git session is going on..");
-		
+		System.out.println("corejava program session");
 
 	}
 
